889682cd94b68447bc5fdb4ef6c0d7f34bad8ebc
rozx <lisida900710@gmail.com>
2016-04-01 14:09:34 -0400

    0.0.1 setting things up

--- README.md
+++ README.md
@@ -2,7 +2,9 @@
 a light novel site for kindle.
 
 
-= v0.0.1 = 
+## v0.0.1
 
 init version with github, finally getting it started!
+
++ server setup!
 
--- main.js
+++ main.js
@@ -1,19 +1,65 @@
+console.log("initializing...");
+
+// init vars
+
 var express = require('express');
 var app = express();
 var port = 80;
 
-console.log("initializing...");
+// init file system
+var fs = require('fs');
+
+// init request 
+var rq = require('request');
+
+//init cheerio
+
+var cheerio = require('cheerio');
+
+
+
+console.log("initialized.");
+
+// listening to port
+
+try{
+
+app.listen(port);
+
+console.log("listening on port:" + port);
+
+} catch (error){
+
+console.error("an error just got caught! Maybe you should run it with sudo?");
+console.error(error);
+
+}
+
+// app config
+
+app.set('view engine', 'jade');
+app.use(express.static('public'));
+app.use(express.static('data'));
+
+// index
 
 app.get('/', function (req, res) {
+                                                                                                                                                     
+  res.render('index',{title: 'Kindle Light Novel',message: 'hello world!'});                                                                                                                                              
+});
 
-  res.send('Hello World');
+// grab web content
+app.param('id',function(req,res,next,id){
+
+	console.log('Getting book id:' + id);
+	next();
 
 });
 
 
-app.listen(port);
+app.get('/book/:id',function(req,res,next){
 
+	res.send("yes!" + req.params.id);
 
-console.log("listening on port:" + port);
-
-
+	
+});
