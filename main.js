11de459c5a04ad6c93e6375708cb92cf5db69c1f
rozx <lisida900710@gmail.com>
2016-04-01 16:35:21 -0400

    finished login function.

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-console.log("initializing...");
+console.log("> initializing...");
 
 // init vars
 
@@ -10,30 +10,36 @@ var port = 80;
 var fs = require('fs');
 
 // init request 
-var rq = require('request');
+//var rq = require('request');
+var rq = require('request').defaults({jar: true});
 
-//init cheerio
+// init cheerio
 
 var cheerio = require('cheerio');
 
 
 
-console.log("initialized.");
+console.log("> initialized.");
 
 // listening to port
 
 try{
 
-app.listen(port);
+	app.listen(port);
 
-console.log("listening on port:" + port);
+	console.log("> listening on port:" + port);
 
 } catch (error){
 
-console.error("an error just got caught! Maybe you should run it with sudo?");
-console.error(error);
+	console.error("> an error just got caught! Maybe you should run it with sudo?");
+	console.error('> ',error);
 
 }
+
+// logging in
+
+login('rozx','1990710');
+
 
 // app config
 
@@ -45,13 +51,13 @@ app.use(express.static('data'));
 
 app.get('/', function (req, res) {
                                                                                                                                                      
-  res.render('index',{title: 'Kindle Light Novel',message: 'hello world!'});                                                                                                                                              
+	res.render('index',{title: 'Kindle Light Novel',message: 'hello world!'});                                                                                                                                              
 });
 
 // grab web content
 app.param('id',function(req,res,next,id){
 
-	console.log('Getting book id:' + id);
+	console.log('> Getting book id:' + id);
 	next();
 
 });
@@ -59,7 +65,63 @@ app.param('id',function(req,res,next,id){
 
 app.get('/book/:id',function(req,res,next){
 
-	res.send("yes!" + req.params.id);
+	var bid = req.params.id;
+	var url = "http://www.wenku8.com/modules/article/packshow.php?id="+ bid  +"&type=txtfull";
+	
+
+	// grab web content
+	rq({url}, function(error, response, html){
+		
+		if(!error){
+			res.send(html);			
+
+		} else {
+			res.send('Error:', error);
+		}
+	});
 
 	
 });
+
+
+// login function
+function login (user,pwd){
+
+	console.log('> Logging in...');
+
+	var postData = {
+                                                                                                                                                     
+                username : user,                                                                                                                     
+                password : pwd,                                                                                                                      
+                usecookie : '315360000',
+		action : 'login'                                                                                                                
+                                                                                                                                                     
+        };
+
+	rq.post({url : 'http://www.wenku8.com/login.php?do=submit',qs: postData},function(err,res,body){
+
+
+		console.log('> Code:',res.statusCode);
+
+		//console.log(body);
+
+
+			
+		if(res.statusCode == 200){
+
+			console.log('> Logged in as ' + user);
+			var cookie = rq.cookie('key1=value1');
+
+			console.log('> Cookies:',cookie);
+			
+			
+		} else {
+			
+			console.log('> Logging failed.');
+
+		}
+
+	});
+};
+
+
